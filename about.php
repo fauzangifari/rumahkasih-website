a7e776aa2aae5d0e989f2fad5069e2ffb44ea19d
Fauzan Gifari <fauzan.gifari30@gmail.com>
2024-03-12 15:22:22 +0800

    updatee

--- about.php
+++ about.php
@@ -2,19 +2,44 @@
 <?php require_once('assets/components/navbar.php'); ?>
 
 <section class="about-page">
-    <div class="overlay">
-        <div class="container">
-            <div class="title">
-                <h2>About Us</h2>
-                <p>
-                    Rumah Kasih is a non-profit organization that provides shelter and care for the homeless and the
-                    needy. We are committed to providing a safe and loving environment for those in need. Our mission is
-                    to provide a safe and loving environment for those in need. We are committed to providing a safe and
-                    loving environment for those in need.
-                </p>
-            </div>
-        </div>
+  <div class="overlay">
+    <div class="container">
+      <div class="title">
+        <h2>About Us</h2>
+        <p>
+          Rumah Kasih isn't just a shelter, it's a lifeline for the homeless and
+          those struggling with hardship. Driven by unwavering compassion, our
+          organization offers a haven for individuals facing difficult
+          circumstances. Rumah Kasih stands as a testament to the power of human kindness. We
+          are dedicated to creating a lasting positive impact on the lives of
+          those most vulnerable in our society.
+        </p>
+      </div>
     </div>
+  </div>
+</section>
+
+<section class="profile">
+  <div class="container">
+    <h2>Profile</h2>
+    <div class="profile-content">
+      <div class="profile-leader">
+        <h2>Muhammad Fauzan Gifari</h2>
+        <h3>Founder Rumah Kasih</h3>
+
+        <p>
+          Muhammad Fauzan Gifari is the founder of Rumah Kasih. He has been
+          working with the homeless and the needy for over 3 years. He is
+          committed to providing a safe and loving environment for those in
+          need. He is committed to providing a safe and loving environment for
+          those in need.
+        </p>
+      </div>
+      <div class="profile-img">
+        <img src="./assets/img/founder.jpg" alt="Founder" />
+      </div>
+    </div>
+  </div>
 </section>
 
 <script src="./assets/js/script.js"></script>
